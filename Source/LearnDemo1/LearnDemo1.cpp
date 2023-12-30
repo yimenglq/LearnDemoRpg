387b0953e0511be65c514f548cbf5935e49d6266
梦都 <2245548883@qq.com>
2023-12-30 17:01:57 +0800

    InitialProject

---
+++ Source/LearnDemo1/LearnDemo1.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "LearnDemo1.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, LearnDemo1, "LearnDemo1" );
