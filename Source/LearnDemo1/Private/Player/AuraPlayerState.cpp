83f1158b4edd8f6c43ff1a583b1f50e7a92d551b
梦都 <2245548883@qq.com>
2024-01-05 15:03:47 +0800

    GAS未使用前

---
+++ Source/LearnDemo1/Private/Player/AuraPlayerState.cpp
@@ -0,0 +1,10 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Player/AuraPlayerState.h"
+
+AAuraPlayerState::AAuraPlayerState()
+{
+
+	NetUpdateFrequency = 100.f;//  考虑复制此执行组件的频率（每秒）
+}
---
+++ Source/LearnDemo1/Public/Player/AuraPlayerState.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "AuraPlayerState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LEARNDEMO1_API AAuraPlayerState : public APlayerState
+{
+	GENERATED_BODY()
+
+public:
+	AAuraPlayerState();
+
+	
+};
